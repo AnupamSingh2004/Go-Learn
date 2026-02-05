4294a7666f425f2ffb342e87bf9a07004e9bfb33
AnupamSingh2004 <sanupam2004@gmail.com>
2026-02-05 11:03:23 +0530

    Erros

---
+++ errors-learn/wrapping-error/go.mod
@@ -0,0 +1,3 @@
+module wrapping-error
+
+go 1.22.2
---
+++ errors-learn/wrapping-error/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func readConfig() error {
+	return errors.New("Simulating error")
+}
+
+func loadApp() error {
+	err := readConfig()
+
+	if err != nil {
+		return fmt.Errorf("load app failed: %w", err)
+	}
+
+	return nil
+}
+
+func main() {
+	err := loadApp()
+
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+}
